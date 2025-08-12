7c3af4ecc67024fcc5c28783cb54b2955242ef6c
Renata Gasior <renatta.gasior@gmail.com>
2025-08-12 18:56:34 +0200

    admin profile updated

--- my_little_pantry/my_fridge/admin.py
+++ my_little_pantry/my_fridge/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, DefaultProduct, FridgeProduct
 
 admin.site.register(Product)
+admin.site.register(DefaultProduct)
+admin.site.register(FridgeProduct)
 
 # Register your models here.
